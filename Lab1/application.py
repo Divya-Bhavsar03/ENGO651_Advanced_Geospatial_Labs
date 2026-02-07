b235586ddaa9e2f30cd1db682f65cbadb5709dd2
Divya-Bhavsar03 <divya.bhavsar@ucalgary.ca>
2026-02-07 16:16:24 -0700

    implemented the regisration logic and tested in database successfully

--- Lab1/application.py
+++ Lab1/application.py
@@ -1,10 +1,11 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from dotenv import load_dotenv
+from werkzeug.security import check_password_hash, generate_password_hash
 
 load_dotenv()
 
@@ -28,6 +29,28 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET","POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if not username:
+            return "Username is missing!"
+        if not password:
+            return "Password is missing!"
+        
+        check_user = db.execute(text("SELECT * FROM users WHERE username=:username"),{"username":username}).fetchone()
+
+        if check_user:
+            return "This username has already been taken. Choose another."
+        
+        hashed_password = generate_password_hash(password)
+
+        db.execute(text("INSERT INTO users (username, password) VALUES(:username, :password)"), {"username":username, "password":hashed_password})
+        db.commit()
+
+        return "Registration Successful! Proceed to Login!"
+
+    else:
+        return render_template("register.html")
